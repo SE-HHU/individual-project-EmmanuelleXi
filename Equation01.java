dd91ffd99fc9daec7ef13baa3f2179386b8c6067
EmmanuelleXi <yie944415@gmail.com>
2022-09-25 14:15:06 +0800

    Update Equation01.java

--- Equation01.java
+++ Equation01.java
@@ -1,13 +1,13 @@
 package work01;
 
 import java.util.*;
-public class Equation01 {
+public class Equation01 {           //class Equation01 做两位数运算
     Random r = new Random();
     int x = r.nextInt(10, 100);
     int y = r.nextInt(10, 100);
     int ans = 0;
     int flag = 0;
-    public void add() {
+    public void add() {                 
         while((x+y)>100) {
             x = r.nextInt(10, 100);
             y = r.nextInt(10, 100);
@@ -25,7 +25,7 @@ public class Equation01 {
         ans = x -y;
         flag = 1;
     }
-    public void creat(){
+    public void creat(){             //把 add（）和 sub（）包装到create（）里，增加代码简洁性
         if(r.nextBoolean())
             this.add();
         else
@@ -40,7 +40,7 @@ public class Equation01 {
     }
     public String toString(){
         StringBuilder stb = new StringBuilder("");
-        if(this.flag == 0)
+        if(this.flag == 0)                                        //把算式存入tostring（）
             stb.append(x+"+"+y+"=");
         else if(this.flag == 1)
             stb.append(x+"-"+y+"=");
